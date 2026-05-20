397aa47003f091b62995a715613f6951adfbc2c1
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-20 19:57:46 +0900

    secretKey를 인코딩하는 메서드 구현

--- src/main/java/com/example/demo/jwt/JwtTokenizer.java
+++ src/main/java/com/example/demo/jwt/JwtTokenizer.java
@@ -1,8 +1,12 @@
 package com.example.demo.jwt;
 
+import io.jsonwebtoken.io.Encoders;
+
+import java.nio.charset.StandardCharsets;
+
 public class JwtTokenizer {
 
     public String encodeBase64SecretKey(String secretKey) {
-
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 }

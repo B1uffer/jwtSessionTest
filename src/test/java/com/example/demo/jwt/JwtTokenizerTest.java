fb2e43de32932f322592e1bab2cee30b159c5b3e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-21 20:47:27 +0900

    @BeforeAll init() 메서드 구현

--- src/test/java/com/example/demo/jwt/JwtTokenizerTest.java
+++ src/test/java/com/example/demo/jwt/JwtTokenizerTest.java
@@ -1,4 +1,20 @@
 package com.example.demo.jwt;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JwtTokenizerTest {
+    private static JwtTokenizer jwtTokenizer;
+    private String secretKey;
+    private String base64EncodedSecretKey;
+
+    @BeforeAll
+    public void init() {
+        jwtTokenizer = new JwtTokenizer();
+        secretKey = "kevin1234123412341234123412341234";
+
+        // a2V2aW4xMjM0MTIzNDEyMzQxMjM0MTIzNDEyMzQxMjM0
+        base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(secretKey);
+    }
 }

af52c05aff2f804ff9e78f15d353b6eb8add2902
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-24 18:01:23 +0900

    JWT 설정 예제 프로젝트 OrderCoffee 엔티티 구현

--- src/main/java/com/example/demo/order/entity/OrderCoffee.java
+++ src/main/java/com/example/demo/order/entity/OrderCoffee.java
@@ -1,4 +1,48 @@
 package com.example.demo.order.entity;
 
+import com.example.demo.coffee.entity.Coffee;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class OrderCoffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderCoffeeId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    /**
+     * order의 pk를 외래키로 들고 있음, 이쪽이 다
+     * 주문 하나에 커피주문 여러개가 있을 수 있다
+     * order는 orderCoffee를 몰라도 되지만, orderCoffee는 order를 알아야한다
+     */
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "COFFEE_ID")
+    private Coffee coffee;
+
+    public void addOrder(Order order) {
+        this.order = order;
+    }
+
+
+    public void addCoffee(Coffee coffee) {
+        this.coffee = coffee;
+    }
 }

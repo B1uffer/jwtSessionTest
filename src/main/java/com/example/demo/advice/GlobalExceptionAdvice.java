c5bfc8b24f3c3d734d00d979ff0d9dfa0bae022e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-24 15:49:56 +0900

    JWT 설정 예제 프로젝트 GlobalExceptionAdvice 구현

--- src/main/java/com/example/demo/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/example/demo/advice/GlobalExceptionAdvice.java
@@ -1,4 +1,10 @@
 package com.example.demo.advice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionAdvice {
+
 }

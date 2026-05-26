bf7d12b9ff714d6e71f459e2cd5494e3ff3c5dbf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-27 02:21:37 +0900

    JWT 설정 예제 프로젝트 NotSpace @interface, NotSpaceValidator extends ConstraintValidator 구현

--- src/main/java/com/example/demo/validator/NotSpaceValidator.java
+++ src/main/java/com/example/demo/validator/NotSpaceValidator.java
@@ -1,4 +1,18 @@
 package com.example.demo.validator;
 
-public class NotSpaceValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.util.StringUtils;
+
+public class NotSpaceValidator implements ConstraintValidator<NotSpace, String> {
+    @Override
+    public void initialize(NotSpace constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        return value == null || StringUtils.hasText(value);
+    }
 }

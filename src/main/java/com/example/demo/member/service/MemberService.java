7bfad4b19990786ccbef386aa56f3280371952bf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-27 04:28:43 +0900

    JWT 설정 예제 프로젝트 MemberService 인터페이스 정의

--- src/main/java/com/example/demo/member/service/MemberService.java
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -1,4 +1,13 @@
 package com.example.demo.member.service;
 
+import com.example.demo.member.entity.Member;
+
 public interface MemberService {
+    Member createMember(Member member);
+    Member updateMember(Member member);
+    Member findMember(long memberId);
+    void deleteMember(long memberId);
+
+    Member findVerifiedMember(long memberId);
+    void verifyExistsEmail(String email);
 }

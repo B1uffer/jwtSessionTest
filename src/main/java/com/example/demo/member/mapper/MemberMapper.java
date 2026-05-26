ef0660e19268a62221366ee5ae5a191eeec87bfd
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-27 04:24:47 +0900

    JWT 설정 예제 프로젝트 MemberMapper 구현

--- src/main/java/com/example/demo/member/mapper/MemberMapper.java
+++ src/main/java/com/example/demo/member/mapper/MemberMapper.java
@@ -1,4 +1,31 @@
 package com.example.demo.member.mapper;
 
-public class MemberMapper {
+import com.example.demo.member.dto.MemberDto;
+import com.example.demo.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberMapper {
+    /**
+     * MemberPost를 Member로 반환
+     */
+    Member memberPostToMember(MemberDto.Post requestBody);
+
+    /**
+     * MemberPatch를 Member로 반환
+     */
+    Member memberPatchToMember(MemberDto.Patch requestBody);
+
+    /**
+     * Member를 MemberDto의 Response로 반환
+     */
+    MemberDto.Response memberToMemberResponse(Member member);
+
+    /**
+     * List<Member>를 List<MemberDto.Response> 로 반환
+     */
+    List<MemberDto.Response> membersToMemberResponses(List<Member> members);
 }

f4a5335784f825616c9e5d513853cf8c6543fb14
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-26 11:03:46 +0900

    JWT 설정 예제 프로젝트 Stamp 엔티티 구현

--- src/main/java/com/example/demo/stamp/Stamp.java
+++ src/main/java/com/example/demo/stamp/Stamp.java
@@ -1,4 +1,43 @@
 package com.example.demo.stamp;
 
+import com.example.demo.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class Stamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stampId;
+
+    /**
+     * 스탬프 카운트
+     */
+    @Column(nullable = false)
+    private int stampCount;
+
+    /**
+     * OneToOne으로 Member랑 조인
+     */
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+        if(member.getStamp() != this) {
+            member.setStamp(this);
+        }
+    }
 }

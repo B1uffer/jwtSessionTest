73f5db705c0644ba79412cf107bc18e1215dd271
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-26 12:13:30 +0900

    JWT 설정 예제 프로젝트 Order 엔티티 구현

--- src/main/java/com/example/demo/order/entity/Order.java
+++ src/main/java/com/example/demo/order/entity/Order.java
@@ -1,5 +1,7 @@
 package com.example.demo.order.entity;
 
+import com.example.demo.coffee.entity.Coffee;
+import com.example.demo.member.entity.Member;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,20 +28,42 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
+    /**
+     * 주문 상태
+     */
+    @Enumerated(EnumType.STRING)
+    private OrderStauts orderStatus = OrderStauts.ORDER_REQUEST; // 디폴트
+
+    /**
+     * 다대일 멤버
+     */
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    /**
+     * orderCoffee의 상태를 알고있음
+     */
     @OneToMany(mappedBy = "order", cascade = CascadeType.PERSIST)
     private List<OrderCoffee> orderCoffees = new ArrayList<>();
 
-    // 커피 주문 추가하기
+
+    /**
+     * setter
+     */
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    /**
+     * 커피주문 추가
+     */
     public void addOrderCoffee(OrderCoffee orderCoffee) {
         this.orderCoffees.add(orderCoffee);
         if(orderCoffee.getOrder() != this) { // orderCoffee가 바라보는 Order가 지금 Order와 다르다면
             orderCoffee.addOrder(this); // 새로운 OrderCoffee에 order를 넣는다
         }
     }
-
-    // 주문 상태
-    @Enumerated(EnumType.STRING)
-    private OrderStauts orderStatus = OrderStauts.ORDER_REQUEST; // 디폴트
 
     public enum OrderStauts {
         ORDER_REQUEST(1, "주문 요청"), // 주문

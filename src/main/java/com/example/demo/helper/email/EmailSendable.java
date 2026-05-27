8915e333a3ed122f1f7b1db7954fa96868842f08
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-27 20:41:17 +0900

    JWT 설정 예제 프로젝트 오토와이어링 문제 @Component 애너테이션 추가

--- src/main/java/com/example/demo/helper/email/EmailSendable.java
+++ src/main/java/com/example/demo/helper/email/EmailSendable.java
@@ -1,5 +1,8 @@
 package com.example.demo.helper.email;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface EmailSendable {
     void send(String message) throws InterruptedException;
 }

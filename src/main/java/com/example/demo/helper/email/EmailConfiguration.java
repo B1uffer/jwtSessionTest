6f7dba52e5b33e14d139f2b3a35f2285d3b0c630
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-29 05:34:22 +0900

    JWT 설정 예제 프로젝트 이메일 helper EmailConfiguration에 MockEmailSendable() 를 반환하는 메서드 구현

--- src/main/java/com/example/demo/helper/email/EmailConfiguration.java
+++ src/main/java/com/example/demo/helper/email/EmailConfiguration.java
@@ -1,5 +1,12 @@
 package com.example.demo.helper.email;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class EmailConfiguration {
+    @Bean
+    public EmailSendable emailSendable() {
+        return new MockEmailSendable();
+    }
 }

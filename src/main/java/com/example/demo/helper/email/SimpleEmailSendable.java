883a6d687eb9a90958d1162edc6a19e7040e5d11
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-29 05:32:21 +0900

    JWT 설정 예제 프로젝트 이메일 helper EmailSendable implements, 주석 추가

--- src/main/java/com/example/demo/helper/email/SimpleEmailSendable.java
+++ src/main/java/com/example/demo/helper/email/SimpleEmailSendable.java
@@ -1,4 +1,8 @@
 package com.example.demo.helper.email;
 
-public class SimpleEmailSendable {
+public class SimpleEmailSendable implements EmailSendable{
+    @Override
+    public void send(String message) throws InterruptedException {
+        // TODO 간단한 문자열 형태의 이메일 구현에 대한 로직 구성하기
+    }
 }

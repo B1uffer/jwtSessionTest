881aa417a78d3d988e9035cfe4f64332c386274a
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-29 05:35:42 +0900

    JWT 설정 예제 프로젝트 MemberRegistrationApplicationListener 필드 주입

--- src/main/java/com/example/demo/helper/listener/MemberRegistrationApplicationListener.java
+++ src/main/java/com/example/demo/helper/listener/MemberRegistrationApplicationListener.java
@@ -1,4 +1,22 @@
 package com.example.demo.helper.listener;
 
+import com.example.demo.helper.email.EmailSender;
+import com.example.demo.member.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Configuration
+@EnableAsync
 public class MemberRegistrationApplicationListener {
+    private final EmailSender emailSender;
+    private final MemberService memberService;
+
+    public MemberRegistrationApplicationListener(EmailSender emailSender, MemberService memberService) {
+        this.emailSender = emailSender;
+        this.memberService = memberService;
+    }
 }

fa712f393f660d067a0cc7aadcac5aebb29cbfaa
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-29 05:40:41 +0900

    JWT 설정 예제 프로젝트 MemberRegistrationApplicationEvent 구현

--- src/main/java/com/example/demo/helper/event/MemberRegistrationApplicationEvent.java
+++ src/main/java/com/example/demo/helper/event/MemberRegistrationApplicationEvent.java
@@ -1,4 +1,22 @@
 package com.example.demo.helper.event;
 
-public class MemberRegistrationApplicationEvent {
+import com.example.demo.member.entity.Member;
+import org.springframework.context.ApplicationEvent;
+
+import java.time.Clock;
+
+public class MemberRegistrationApplicationEvent extends ApplicationEvent {
+    private Member member;
+
+    public MemberRegistrationApplicationEvent(Object source, Member clock) {
+        super(source);
+        this.member = member;
+    }
+
+    /**
+     * getter
+     */
+    public Member getMember() {
+        return member;
+    }
 }

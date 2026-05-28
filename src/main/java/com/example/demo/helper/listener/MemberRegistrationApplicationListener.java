d0a8fbb0607465f162655583d13aa8aafc4c521a
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-29 05:51:32 +0900

    JWT 설정 예제 프로젝트 listen 메서드 구현

--- src/main/java/com/example/demo/helper/listener/MemberRegistrationApplicationListener.java
+++ src/main/java/com/example/demo/helper/listener/MemberRegistrationApplicationListener.java
@@ -2,10 +2,12 @@ package com.example.demo.helper.listener;
 
 import com.example.demo.helper.email.EmailSender;
 import com.example.demo.helper.event.MemberRegistrationApplicationEvent;
+import com.example.demo.member.entity.Member;
 import com.example.demo.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.mail.MailSendException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
@@ -23,9 +25,18 @@ public class MemberRegistrationApplicationListener {
         this.memberService = memberService;
     }
 
+    // 메세지 전송 리스너
     @Async
     @EventListener
-    public void listen(MemberRegistrationApplicationEvent event) {
-
+    public void listen(MemberRegistrationApplicationEvent event) throws InterruptedException {
+        try {
+            String message = "전송하려는 메시지";
+            emailSender.sendMail(message);
+        } catch(MailSendException e) {
+            e.printStackTrace();
+            log.error("MailSendException 발생", e);
+            Member member = event.getMember();
+            memberService.deleteMember(member.getMemberId()); // UX면에서 위험할 수 있음
+        }
     }
 }

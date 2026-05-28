b9c5e6b9d6bb7024ead884af20ce049773fe71e0
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-05-29 05:41:47 +0900

    JWT 설정 예제 프로젝트 listen 메서드 생성

--- src/main/java/com/example/demo/helper/listener/MemberRegistrationApplicationListener.java
+++ src/main/java/com/example/demo/helper/listener/MemberRegistrationApplicationListener.java
@@ -1,9 +1,12 @@
 package com.example.demo.helper.listener;
 
 import com.example.demo.helper.email.EmailSender;
+import com.example.demo.helper.event.MemberRegistrationApplicationEvent;
 import com.example.demo.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
@@ -18,5 +21,11 @@ public class MemberRegistrationApplicationListener {
     public MemberRegistrationApplicationListener(EmailSender emailSender, MemberService memberService) {
         this.emailSender = emailSender;
         this.memberService = memberService;
+    }
+
+    @Async
+    @EventListener
+    public void listen(MemberRegistrationApplicationEvent event) {
+
     }
 }

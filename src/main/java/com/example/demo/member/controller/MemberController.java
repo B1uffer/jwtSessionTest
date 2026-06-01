d18581b6259f346a5cf046cf9433056fa1d21931
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 13:08:24 +0900

    JWT 설정 예제 프로젝트 MemberController getMember() 메서드 생성

--- src/main/java/com/example/demo/member/controller/MemberController.java
+++ src/main/java/com/example/demo/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,10 @@ public class MemberController {
         memberService.createMember(member);
         URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, member.getMemberId()); // 신규 회원의 URI 생성
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{member_id}")
+    public ResponseEntity getMember() {
+        return null;
     }
 }

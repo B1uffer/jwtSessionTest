b57c0508fea414139d600ff8f7bef7e285848a96
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 14:22:33 +0900

    JWT 설정 예제 프로젝트 MemberService Page<Member> 타입의 findMembers() 선언

--- src/main/java/com/example/demo/member/service/MemberService.java
+++ src/main/java/com/example/demo/member/service/MemberService.java
@@ -1,10 +1,12 @@
 package com.example.demo.member.service;
 
 import com.example.demo.member.entity.Member;
+import org.springframework.data.domain.Page;
 
 public interface MemberService {
     Member createMember(Member member);
     Member findMember(long memberId);
+    Page<Member> findMembers(int page, int size);
     Member updateMember(Member member);
     void deleteMember(long memberId);
 

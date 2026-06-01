c57b14bdc6b7dff2d3d6228761c4fbc314a94ebd
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 19:53:20 +0900

    JWT 설정 예제 프로젝트 CoffeeResponseDto 구현

--- src/main/java/com/example/demo/coffee/dto/CoffeeResponseDto.java
+++ src/main/java/com/example/demo/coffee/dto/CoffeeResponseDto.java
@@ -1,4 +1,21 @@
 package com.example.demo.coffee.dto;
 
+import com.example.demo.coffee.entity.Coffee;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class CoffeeResponseDto {
+    private long coffeeId;
+
+    private String korName;
+
+    private String engName;
+
+    private int price;
+
+    private String coffeeCode;
+
+    private Coffee.CoffeeStatus coffeeStatus;
 }

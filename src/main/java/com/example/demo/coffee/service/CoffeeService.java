1719398d32cec177d6307914b1326b8a290a8008
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 20:17:59 +0900

    JWT 설정 예제 프로젝트 CoffeeService, BasicCoffeeService 클래스 생성

---
+++ src/main/java/com/example/demo/coffee/service/CoffeeService.java
@@ -0,0 +1,4 @@
+package com.example.demo.coffee.service;
+
+public interface CoffeeService {
+}
---
+++ src/main/java/com/example/demo/coffee/service/basic/BasicCoffeeService.java
@@ -0,0 +1,8 @@
+package com.example.demo.coffee.service.basic;
+
+import com.example.demo.coffee.service.CoffeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BasicCoffeeService implements CoffeeService {
+}

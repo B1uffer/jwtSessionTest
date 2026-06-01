1c8778d30bffac2ed53afae319862f77a4824650
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 20:50:55 +0900

    JWT 설정 예제 프로젝트 BasicCoffeeService implements 구현체 생성

--- src/main/java/com/example/demo/coffee/service/basic/BasicCoffeeService.java
+++ src/main/java/com/example/demo/coffee/service/basic/BasicCoffeeService.java
@@ -1,8 +1,44 @@
 package com.example.demo.coffee.service.basic;
 
+import com.example.demo.coffee.entity.Coffee;
 import com.example.demo.coffee.service.CoffeeService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BasicCoffeeService implements CoffeeService {
+    @Override
+    public Coffee createCoffee(Coffee coffee) {
+        return null;
+    }
+
+    @Override
+    public Coffee findCoffee(long coffeeId) {
+        return null;
+    }
+
+    @Override
+    public Page<Coffee> findCoffees(int page, int size) {
+        return null;
+    }
+
+    @Override
+    public Coffee updateCoffee(Coffee coffee) {
+        return null;
+    }
+
+    @Override
+    public void deleteCoffee(long coffeeId) {
+
+    }
+
+    @Override
+    public void verifyExistsCoffee(String coffeeCode) {
+
+    }
+
+    @Override
+    public Coffee findVerifiedCoffee(long coffeeId) {
+        return null;
+    }
 }

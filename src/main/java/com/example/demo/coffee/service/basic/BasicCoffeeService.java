7deac16e5da7785f84f9f574e22174a5b0308dc5
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 20:51:44 +0900

    JWT 설정 예제 프로젝트 repository 필드 주입, 생성자 구현

--- src/main/java/com/example/demo/coffee/service/basic/BasicCoffeeService.java
+++ src/main/java/com/example/demo/coffee/service/basic/BasicCoffeeService.java
@@ -1,12 +1,19 @@
 package com.example.demo.coffee.service.basic;
 
 import com.example.demo.coffee.entity.Coffee;
+import com.example.demo.coffee.repository.CoffeeRepository;
 import com.example.demo.coffee.service.CoffeeService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BasicCoffeeService implements CoffeeService {
+    private final CoffeeRepository coffeeRepository;
+
+    public BasicCoffeeService(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
     @Override
     public Coffee createCoffee(Coffee coffee) {
         return null;

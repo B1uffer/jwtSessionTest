42036a0551e1374b1b24cafd72a590e8c524123f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-01 20:50:05 +0900

    JWT 설정 예제 프로젝트 CoffeeService 인터페이스 구현

--- src/main/java/com/example/demo/coffee/service/CoffeeService.java
+++ src/main/java/com/example/demo/coffee/service/CoffeeService.java
@@ -1,4 +1,15 @@
 package com.example.demo.coffee.service;
 
+import com.example.demo.coffee.entity.Coffee;
+import org.springframework.data.domain.Page;
+
 public interface CoffeeService {
+    Coffee createCoffee(Coffee coffee);
+    Coffee findCoffee(long coffeeId);
+    Page<Coffee> findCoffees(int page, int size);
+    Coffee updateCoffee(Coffee coffee);
+    void deleteCoffee(long coffeeId);
+
+    void verifyExistsCoffee(String coffeeCode);
+    Coffee findVerifiedCoffee(long coffeeId);
 }

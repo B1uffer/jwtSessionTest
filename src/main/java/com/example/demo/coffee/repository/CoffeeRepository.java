7ecd0838f80397c24e9041ee29a3b0c032615446
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-03 22:42:48 +0900

    JWT 설정 예제 프로젝트 Coffee 엔티티에 있는 coffeeCode로 coffee찾기 findByCoffeeCode() 메서드 추가

--- src/main/java/com/example/demo/coffee/repository/CoffeeRepository.java
+++ src/main/java/com/example/demo/coffee/repository/CoffeeRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.coffee.repository;
 import com.example.demo.coffee.entity.Coffee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    Optional<Coffee> findByCoffeeCode(String coffeeCode);
 }

fbc2cdf186fdd9522a2719d245cab46457933f8d
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-04 20:05:03 +0900

    JWT 설정 예제 프로젝트 getCoffee() 메서드 생성

--- src/main/java/com/example/demo/coffee/controller/CoffeeController.java
+++ src/main/java/com/example/demo/coffee/controller/CoffeeController.java
@@ -7,8 +7,11 @@ import com.example.demo.coffee.repository.CoffeeRepository;
 import com.example.demo.coffee.service.CoffeeService;
 import com.example.demo.utils.UriCreator;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +37,10 @@ public class CoffeeController {
         Coffee coffee = coffeeService.createCoffee(coffeeMapper.coffeePostToCoffee(coffeePostDto));
         URI location = UriCreator.createUri(COFFFEE_DEFAULT_URL, coffee.getCoffeeId());
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{coffee_id}")
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") @Positive long coffeeId) {
+        return null;
     }
 }

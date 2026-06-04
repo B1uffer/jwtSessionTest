9d605d2cfa0cfc231089a06923a5dae4df3b5b34
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-04 20:38:30 +0900

    JWT 설정 예제 프로젝트 deleteCoffee() 메서드 생성

--- src/main/java/com/example/demo/coffee/controller/CoffeeController.java
+++ src/main/java/com/example/demo/coffee/controller/CoffeeController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,5 +76,10 @@ public class CoffeeController {
         return new ResponseEntity<>(
                 new SingleResponseDto<>(coffee), HttpStatus.OK
         );
+    }
+
+    @DeleteMapping("/{coffee-id}")
+    public ResponseEntity deleteCoffee() {
+        return null;
     }
 }

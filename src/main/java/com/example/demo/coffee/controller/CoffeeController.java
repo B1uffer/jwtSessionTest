8152f76ae912f239b03139ee224cf2f43fd1f9d6
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-04 19:47:19 +0900

    JWT 설정 예제 프로젝트 Service, Mapper 필드 주입 및 URL 필드 생성 postCoffee() 메서드 생성

--- src/main/java/com/example/demo/coffee/controller/CoffeeController.java
+++ src/main/java/com/example/demo/coffee/controller/CoffeeController.java
@@ -1,4 +1,31 @@
 package com.example.demo.coffee.controller;
 
+import com.example.demo.coffee.dto.CoffeePostDto;
+import com.example.demo.coffee.mapper.CoffeeMapper;
+import com.example.demo.coffee.service.CoffeeService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v11/coffees")
+@Validated
 public class CoffeeController {
+    private final String COFFFEE_DEFAULT_URL = "/v11/coffees";
+    private final CoffeeService coffeeService;
+    private final CoffeeMapper coffeeMapper;
+
+    public CoffeeController(CoffeeService coffeeService, CoffeeMapper coffeeMapper) {
+        this.coffeeService = coffeeService;
+        this.coffeeMapper = coffeeMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity postCoffee(@Valid @RequestBody CoffeePostDto coffeePostDto) {
+        return null;
+    }
 }

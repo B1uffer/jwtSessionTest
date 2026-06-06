da7a33ff8d72d51ddb09af9c126d0564348afd72
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-06 23:22:06 +0900

    JWT 설정 예제 프로젝트 OrderPatchDto 구현

--- src/main/java/com/example/demo/order/dto/OrderPatchDto.java
+++ src/main/java/com/example/demo/order/dto/OrderPatchDto.java
@@ -1,4 +1,14 @@
 package com.example.demo.order.dto;
 
+import com.example.demo.order.entity.Order;
+import lombok.Getter;
+
+@Getter
 public class OrderPatchDto {
+    private long orderId;
+    private Order.OrderStauts orderStatus;
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
 }

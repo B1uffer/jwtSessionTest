a88fcd2e82c29891e10feecae7498814a325a91e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-06 23:21:06 +0900

    JWT 설정 예제 프로젝트 OrderPostDto 구현

--- src/main/java/com/example/demo/order/dto/OrderPostDto.java
+++ src/main/java/com/example/demo/order/dto/OrderPostDto.java
@@ -1,4 +1,24 @@
 package com.example.demo.order.dto;
 
+import com.example.demo.member.entity.Member;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class OrderPostDto {
+    @Positive
+    private long memberId;
+
+    @Valid
+    private List<OrderCoffeeDto> orderCoffees;
+
+    // OrderPostDto의 memberId 필드를 통한 member 얻기
+    public Member getMember() {
+        Member member = new Member();
+        member.setMemberId(memberId);
+        return member;
+    }
 }

18cb12cb4e7eed295cd3f2dba8a19c31faa43f92
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-06 23:24:18 +0900

    JWT 설정 예제 프로젝트 OrderResponseDto 구현

--- src/main/java/com/example/demo/order/dto/OrderResponseDto.java
+++ src/main/java/com/example/demo/order/dto/OrderResponseDto.java
@@ -1,4 +1,26 @@
 package com.example.demo.order.dto;
 
+import com.example.demo.member.entity.Member;
+import com.example.demo.order.entity.Order;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
 public class OrderResponseDto {
+    private long orderId;
+
+    @Setter(AccessLevel.NONE)
+    private long memberId;
+    private Order.OrderStauts orderStatus;
+    private List<OrderCoffeeResponseDto> orderCoffees;
+    private LocalDateTime createdAt;
+
+    public void setMemberId(Member member) {
+        this.memberId = member.getMemberId();
+    }
 }

f76ff755b5a84267384c936c45be8c90405f1b04
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-06 23:26:32 +0900

    JWT 설정 예제 프로젝트 OrderCoffeeDto, OrderCoffeeResponseDto 구현

--- src/main/java/com/example/demo/order/dto/OrderCoffeeDto.java
+++ src/main/java/com/example/demo/order/dto/OrderCoffeeDto.java
@@ -1,4 +1,13 @@
 package com.example.demo.order.dto;
 
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+
+@Getter
 public class OrderCoffeeDto {
+    @Positive
+    private long coffeeId;
+
+    @Positive
+    private int quantity;
 }
--- src/main/java/com/example/demo/order/dto/OrderCoffeeResponseDto.java
+++ src/main/java/com/example/demo/order/dto/OrderCoffeeResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.demo.order.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class OrderCoffeeResponseDto {
+    private long coffeeId;
+    private Integer quantity;
+    private String korName;
+    private String engName;
+    private Integer price;
 }

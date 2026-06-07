7ef4d13298d7de26942e2a0bd3beb047fee4f320
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-07 13:59:23 +0900

    JWT 설정 예제 프로젝트 OrderService, BasicOrderService 클래스 생성

---
+++ src/main/java/com/example/demo/order/service/basic/BasicOrderService.java
@@ -0,0 +1,9 @@
+package com.example.demo.order.service.basic;
+
+import com.example.demo.order.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BasicOrderService implements OrderService {
+
+}

6a64e2f61b4803d3197a795b6809ab1f41ff3f7f
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-07 13:16:41 +0900

    JWT 설정 예제 프로젝트 ORM OrderPatchDto를 Order로 바꾸는 Mapper orderPatchDtoToOrder
    orderPostDto를 Order로 바꾸는 Mapper
    orderToOrderResponseDtoList 인터페이스 생성
    
    OrderPostDto를 Order로 바꾸는 Mapper
    orderPostDtoToOrder 인터페이스 생성
    default로 생성하여 메서드 내부에서 OrderCoffee 생성 후 order에 넣음

--- src/main/java/com/example/demo/order/mapper/OrderMapper.java
+++ src/main/java/com/example/demo/order/mapper/OrderMapper.java
@@ -1,4 +1,41 @@
 package com.example.demo.order.mapper;
 
-public class OrderMapper {
+import com.example.demo.coffee.entity.Coffee;
+import com.example.demo.member.entity.Member;
+import com.example.demo.order.dto.OrderPatchDto;
+import com.example.demo.order.dto.OrderPostDto;
+import com.example.demo.order.dto.OrderResponseDto;
+import com.example.demo.order.entity.Order;
+import com.example.demo.order.entity.OrderCoffee;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+    Order orderPathDtoToOrder(OrderPatchDto orderPatchDto);
+    List<OrderResponseDto> orderToOrderResponseDtoList(List<Order> orderList);
+
+    default Order orderPostDtoToOrder(OrderPostDto orderPostDto) {
+        Order order = new Order();
+        Member member = new Member();
+        member.setMemberId(order.getOrderId());
+
+        List<OrderCoffee> orderCoffees = orderPostDto.getOrderCoffees().stream()
+                .map(orderCoffeeDto -> {
+                    OrderCoffee orderCoffee = new OrderCoffee();
+                    Coffee coffee = new Coffee();
+                    coffee.setCoffeeId(orderCoffeeDto.getCoffeeId());
+                    orderCoffee.setOrder(order);
+                    orderCoffee.setQuantity(orderCoffeeDto.getQuantity());
+                    orderCoffee.setCoffee(coffee);
+                    return orderCoffee;
+                }).collect(Collectors.toList()); // toList();
+
+        order.setMember(member);
+        order.setOrderCoffees(orderCoffees);
+
+        return order;
+    }
 }

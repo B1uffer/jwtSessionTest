33356e4ae0dbb9c76684f011642fbfcb65699843
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-07 14:37:48 +0900

    JWT 설정 예제 프로젝트 OrderService 인터페이스 구현

--- src/main/java/com/example/demo/order/service/OrderService.java
+++ src/main/java/com/example/demo/order/service/OrderService.java
@@ -1,4 +1,28 @@
 package com.example.demo.order.service;
 
+import com.example.demo.order.entity.Order;
+import org.springframework.data.domain.Page;
+
 public interface OrderService {
+    Order createOrder(Order order);
+    Order findOrder(long orderId);
+    Page<Order> findOrders(int page, int size);
+    Order updateOrder(Order order);
+    void deleteOrder(long orderId);
+
+    void cancelOrder(long orderId);
+
+    /**
+     * 검증 및 stamp 관리, saveOrder는 private로
+     */
+    Order findVerifiedOrderId(long orderId);
+    void verifyOrder(Order order);
+
+    /**
+     * Order를 받아서 stamp 관리하기
+     */
+    void updateStamp(Order order);
+    int calculateStampCount(Order order);
+
+    void saveOrder(Order order);
 }

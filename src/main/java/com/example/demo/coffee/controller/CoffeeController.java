314bba04c5f11788ce1f2ef4fb58dcd5bef7485e
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-08 17:46:38 +0900

    JWT 설정 예제 프로젝트 CoffeeMapper patchCoffee의 애너테이션을 GetMapping에서 PatchMapping으로 수정
    가독성을 위해 줄 정리

--- src/main/java/com/example/demo/coffee/controller/CoffeeController.java
+++ src/main/java/com/example/demo/coffee/controller/CoffeeController.java
@@ -4,7 +4,6 @@ import com.example.demo.coffee.dto.CoffeePatchDto;
 import com.example.demo.coffee.dto.CoffeePostDto;
 import com.example.demo.coffee.entity.Coffee;
 import com.example.demo.coffee.mapper.CoffeeMapper;
-import com.example.demo.coffee.repository.CoffeeRepository;
 import com.example.demo.coffee.service.CoffeeService;
 import com.example.demo.dto.MultiResponseDto;
 import com.example.demo.dto.SingleResponseDto;
@@ -15,14 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
@@ -35,7 +27,7 @@ public class CoffeeController {
     private final CoffeeService coffeeService;
     private final CoffeeMapper coffeeMapper;
 
-    public CoffeeController(CoffeeService coffeeService, CoffeeMapper coffeeMapper, CoffeeRepository coffeeRepository) {
+    public CoffeeController(CoffeeService coffeeService, CoffeeMapper coffeeMapper) {
         this.coffeeService = coffeeService;
         this.coffeeMapper = coffeeMapper;
     }
@@ -44,11 +36,13 @@ public class CoffeeController {
     public ResponseEntity postCoffee(@Valid @RequestBody CoffeePostDto coffeePostDto) {
         Coffee coffee = coffeeService.createCoffee(coffeeMapper.coffeePostToCoffee(coffeePostDto));
         URI location = UriCreator.createUri(COFFFEE_DEFAULT_URL, coffee.getCoffeeId());
+
         return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{coffee_id}")
-    public ResponseEntity getCoffee(@PathVariable("coffee-id") @Positive long coffeeId) {
+    public ResponseEntity getCoffee(@PathVariable("coffee-id")
+                                        @Positive long coffeeId) {
         Coffee coffee = coffeeService.findCoffee(coffeeId);
         return new ResponseEntity<>(
                 new SingleResponseDto<>(coffeeMapper.coffeeToCoffeeResponseDto(coffee)), HttpStatus.OK);
@@ -57,24 +51,27 @@ public class CoffeeController {
     @GetMapping
     public ResponseEntity getAllCoffees(@Positive @RequestParam int page,
                                         @Positive @RequestParam int size) {
-        Page<Coffee> pageCoffees = coffeeService.findCoffees(page, size);
+        Page<Coffee> pageCoffees = coffeeService.findCoffees(page - 1, size);
         List<Coffee> coffees = pageCoffees.getContent();
         return new ResponseEntity<>(
-                new MultiResponseDto<>(coffeeMapper.coffeesToCoffeeResponseDto(coffees), pageCoffees), HttpStatus.OK
+                new MultiResponseDto<>(coffeeMapper.coffeesToCoffeeResponseDto(coffees),
+                        pageCoffees),
+                HttpStatus.OK
         );
     }
 
     // 수정 API
-    @GetMapping("/{coffee-id}")
+    @PatchMapping("/{coffee-id}")
     public ResponseEntity patchCoffee(
-            @Positive @PathVariable("coffee-id") long coffeeId,
-            @Valid @RequestBody CoffeePatchDto requestBody) {
+            @PathVariable("coffee-id") @Positive long coffeeId,
+            @Valid @RequestBody CoffeePatchDto coffeePatchDto) {
 
-        requestBody.setCoffeeId(coffeeId);
-        Coffee coffee = coffeeService.updateCoffee(coffeeMapper.coffeePathToCoffee(requestBody));
+        coffeePatchDto.setCoffeeId(coffeeId);
+        Coffee coffee = coffeeService.updateCoffee(coffeeMapper.coffeePatchToCoffee(coffeePatchDto));
 
         return new ResponseEntity<>(
-                new SingleResponseDto<>(coffee), HttpStatus.OK
+                new SingleResponseDto<>(coffeeMapper.coffeeToCoffeeResponseDto(coffee)),
+                HttpStatus.OK
         );
     }
 

fdea4d86fcb594d16f77d152ca6923c5f6cd4769
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-08 18:28:28 +0900

    JWT 설정 예제 프로젝트 OrderController 클래스 생성

---
+++ src/main/java/com/example/demo/order/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.example.demo.order.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v11/orders")
+@Validated
+public class OrderController {
+}

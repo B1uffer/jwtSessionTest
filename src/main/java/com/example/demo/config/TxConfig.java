b240f22c080aec918b2f46daccbbdf75b6d048e4
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-08 18:17:35 +0900

    JWT 설정 예제 프로젝트 트랜잭션 관련 config, TxConfig txAdvice() 메서드 구현

--- src/main/java/com/example/demo/config/TxConfig.java
+++ src/main/java/com/example/demo/config/TxConfig.java
@@ -4,8 +4,15 @@ import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
+import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
+import org.springframework.transaction.interceptor.TransactionAttribute;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TxConfig {
     // jakarta 아님
@@ -17,7 +24,24 @@ public class TxConfig {
 
     @Bean
     public TransactionInterceptor txAdvice() {
-        return null;
+        NameMatchTransactionAttributeSource txAttributeSource =
+                new NameMatchTransactionAttributeSource();
+        RuleBasedTransactionAttribute txAttribute =
+                new RuleBasedTransactionAttribute();
+        txAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        RuleBasedTransactionAttribute txFindAttribute =
+                new RuleBasedTransactionAttribute();
+        txFindAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        txFindAttribute.setReadOnly(true);
+
+        Map<String, TransactionAttribute> txMethods = new HashMap<>();
+        txMethods.put("find*", txFindAttribute);
+        txMethods.put("*", txAttribute);
+
+        txAttributeSource.setNameMap(txMethods);
+
+        return new TransactionInterceptor(transactionManager, txAttributeSource);
     }
 
     @Bean

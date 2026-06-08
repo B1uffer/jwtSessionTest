2495b78d15e086d6c02be1aeacd5ad8fc8b444f1
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-08 18:08:48 +0900

    JWT 설정 예제 프로젝트 트랜잭션 관련 config, TxConfig txAdvisor() 메서드 구현

--- src/main/java/com/example/demo/config/TxConfig.java
+++ src/main/java/com/example/demo/config/TxConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
@@ -20,6 +22,8 @@ public class TxConfig {
 
     @Bean
     public Advisor txAdvisor() {
-        return null;
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.codestates.coffee.service." + "CoffeeService.*(..))");
+        return new DefaultPointcutAdvisor(pointcut, txAdvice());
     }
 }

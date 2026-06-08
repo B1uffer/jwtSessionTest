e372ccd2a612aeef19c8f5f1b6acbe267bedc33d
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-08 17:55:34 +0900

    JWT 설정 예제 프로젝트 트랜잭션 관련 config, TxConfig txAdvice(), txAdvisor() 메서드 생성

--- src/main/java/com/example/demo/config/TxConfig.java
+++ src/main/java/com/example/demo/config/TxConfig.java
@@ -1,4 +1,25 @@
 package com.example.demo.config;
 
+import org.springframework.aop.Advisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
 public class TxConfig {
+    // jakarta 아님
+    private final TransactionManager transactionManager;
+
+    public TxConfig(TransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public TransactionInterceptor txAdvice() {
+        return null;
+    }
+
+    @Bean
+    public Advisor txAdvisor() {
+        return null;
+    }
 }

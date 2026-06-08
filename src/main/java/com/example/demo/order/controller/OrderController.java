4e3401a06ba1c3643c758e25af896418b7d41e36
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-08 18:32:08 +0900

    JWT 설정 예제 프로젝트 OrderController 필드 주입

--- src/main/java/com/example/demo/order/controller/OrderController.java
+++ src/main/java/com/example/demo/order/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.example.demo.order.controller;
 
+import com.example.demo.member.service.MemberService;
+import com.example.demo.order.mapper.OrderMapper;
+import com.example.demo.order.service.OrderService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v11/orders")
 @Validated
 public class OrderController {
+    private final static String ORDER_DEFAULT_URL = "/v11/orders";
+    private final OrderService orderService;
+    private final OrderMapper orderMapper;
+    private final MemberService memberService;
+
+    public OrderController(OrderService orderService,
+                           OrderMapper orderMapper,
+                           MemberService memberService) {
+        this.orderService = orderService;
+        this.orderMapper = orderMapper;
+        this.memberService = memberService;
+    }
 }

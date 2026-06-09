f4e59a40d2b3b10d865b1735dc92e70d2da72888
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-09 15:49:20 +0900

    JWT 설정 예제 프로젝트 SecurityConfiguration securityFilterChain 최소 설정

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -1,4 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin())
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(
+                        authorize -> authorize.anyRequest().permitAll()
+                )
+        ;
+        return http.build();
+    }
 }

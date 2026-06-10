fa7c0bf4f6487f7b9b7c2003c899ef6ab0222343
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-10 16:30:21 +0900

    JWT 설정 예제 프로젝트 JWT 자격증명 로그인 인증 구현 Custom UserDetailsService 필드 주입, 인터페이스 구현체 메서드 생성

--- src/main/java/com/example/demo/jwt/MemberDetailsService.java
+++ src/main/java/com/example/demo/jwt/MemberDetailsService.java
@@ -1,4 +1,25 @@
 package com.example.demo.jwt;
 
-public class MemberDetailsService {
+import com.example.demo.member.repository.MemberRepository;
+import com.example.demo.utils.CustomAuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public MemberDetailsService(MemberRepository memberRepository,
+                                CustomAuthorityUtils authorityUtils) {
+        this.memberRepository = memberRepository;
+        this.authorityUtils = authorityUtils;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }

a23cf9e5d2379163a69571399f472c46edf05405
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-10 14:29:09 +0900

    JWT 설정 예제 프로젝트 utils CustomAuthorityUtils 구현

--- src/main/java/com/example/demo/utils/CustomAuthorityUtils.java
+++ src/main/java/com/example/demo/utils/CustomAuthorityUtils.java
@@ -1,4 +1,42 @@
 package com.example.demo.utils;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class CustomAuthorityUtils {
+    private String adminMailAddress = "admin@gmail.com";
+    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
+    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
+
+    private final List<String> ADMIN_ROLES_STRING = List.of("ROLE_ADMIN", "ROLE_USER");
+    private final List<String> USER_ROLES_STRING = List.of("ROLE_USER");
+
+    public List<GrantedAuthority> createAuthorities(String email) {
+        if(email.equals(adminMailAddress)) {
+            return ADMIN_ROLES;
+        }
+        return USER_ROLES;
+    }
+
+    public List<GrantedAuthority> createAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+
+        return authorities;
+    }
+
+    // DB에 저장하는 용도
+    public List<String> createRolese(String email) {
+        if(email.equals(adminMailAddress)) {
+            return ADMIN_ROLES_STRING;
+        }
+        return USER_ROLES_STRING;
+    }
 }

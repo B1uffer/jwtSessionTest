35d6618974578f7161e19cfe70c59fede96b456e
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-10 17:25:37 +0900

    JWT 설정 예제 프로젝트 JWT 자격증명 로그인 인증 구현 Custom UserDetailsService MemberDetailsService 구현

--- src/main/java/com/example/demo/jwt/MemberDetailsService.java
+++ src/main/java/com/example/demo/jwt/MemberDetailsService.java
@@ -1,11 +1,18 @@
 package com.example.demo.jwt;
 
+import com.example.demo.exception.BusinessLogicException;
+import com.example.demo.exception.ExceptionCode;
+import com.example.demo.member.entity.Member;
 import com.example.demo.member.repository.MemberRepository;
 import com.example.demo.utils.CustomAuthorityUtils;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
 
 @Component
 public class MemberDetailsService implements UserDetailsService {
@@ -20,6 +27,52 @@ public class MemberDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member findMember = optionalMember
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new MemberDetails(findMember);
+    }
+
+    private final class MemberDetails extends Member implements UserDetails {
+        MemberDetails(Member member) {
+            setMemberId(member.getMemberId());
+            setEmail(member.getEmail());
+            setPassword(member.getPassword());
+            setRoles(member.getRoles());
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return UserDetails.super.isAccountNonExpired();
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return UserDetails.super.isAccountNonLocked();
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            // return true;
+            return UserDetails.super.isCredentialsNonExpired();
+        }
+
+        @Override
+        public boolean isEnabled() {
+            // return true;
+            return UserDetails.super.isEnabled();
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return authorityUtils.createAuthorities(this.getRoles()); // member의 getRole()
+        }
+
+        @Override
+        public String getUsername() {
+            // return true;
+            return getEmail(); // member의 getEmail()
+        }
     }
 }

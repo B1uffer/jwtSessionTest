c89e8957026e2fe91d896c41410cb154df9bd944
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-11 16:35:18 +0900

    JWT 설정 예제 프로젝트 JWT를 생성하는 JwtTokenizer 구현 encodeBase64SecretKey() 메서드 구현

--- src/main/java/com/example/demo/jwt/JwtTokenizer.java
+++ src/main/java/com/example/demo/jwt/JwtTokenizer.java
@@ -1,8 +1,11 @@
 package com.example.demo.jwt;
 
+import io.jsonwebtoken.io.Encoders;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtTokenizer {
@@ -17,4 +20,9 @@ public class JwtTokenizer {
     @Getter
     @Value("${jwt.refresh-token-expiration-minutes}")
     private int refreshTokenExpirationMinutes;
+
+    // secretKey를 base64로 인코딩하는 메서드
+    public String encodeBase64SecretKey(String secretKey) {
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
 }

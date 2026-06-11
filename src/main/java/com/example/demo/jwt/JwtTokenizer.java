4a228812ce1b2f14b40459c2778894c84887a7ef
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-11 16:29:56 +0900

    JWT 설정 예제 프로젝트 JWT를 생성하는 JwtTokenizer 구현 필드 선언

--- src/main/java/com/example/demo/jwt/JwtTokenizer.java
+++ src/main/java/com/example/demo/jwt/JwtTokenizer.java
@@ -1,4 +1,20 @@
 package com.example.demo.jwt;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtTokenizer {
+    @Getter
+    @Value("${jwt.key}")
+    private String secretKey;
+
+    @Getter
+    @Value("${jwt.access-token-expiration-minutes}")
+    private int accessTokenExpirationMinutes;
+
+    @Getter
+    @Value("${jwt.refresh-token-expiration-minutes}")
+    private int refreshTokenExpirationMinutes;
 }

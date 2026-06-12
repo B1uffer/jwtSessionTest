9ca6dddbcc04db4a200f96657531ea4616ecc5b2
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-12 14:57:45 +0900

    JWT 설정 예제 프로젝트 JWT를 생성하는 JwtTokenizer 구현 로그인 인증 요청을 처리하는 Custom Security Filter 구현 필드 주입

--- src/main/java/com/example/demo/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,16 @@
 package com.example.demo.jwt.filter;
 
-public class JwtAuthenticationFilter {
+import com.example.demo.jwt.JwtTokenizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
+                                   JwtTokenizer jwtTokenizer) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenizer = jwtTokenizer;
+    }
 }

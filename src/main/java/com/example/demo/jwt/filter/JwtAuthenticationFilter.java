dbb8de965dd48707030a98d0bfc39c8f26060b51
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-12 15:03:02 +0900

    JWT 설정 예제 프로젝트 JWT를 생성하는 JwtTokenizer 구현 로그인 인증 요청을 처리하는 Custom Security Filter 구현
    UsernamePasswordAuthentication의 메서드 attemptAuthentication 메서드 override
    AbstractAuthenticationProcessingFilter의 메서드 successfulAuthentication 메서드 override

--- src/main/java/com/example/demo/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,16 @@
 package com.example.demo.jwt.filter;
 
 import com.example.demo.jwt.JwtTokenizer;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
@@ -12,5 +20,18 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                    JwtTokenizer jwtTokenizer) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenizer = jwtTokenizer;
+    }
+
+    // UsernamePasswordAuthenticationFilter의 메서드
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    // AbstractAuthenticationProcessingFilter의 메서드
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }

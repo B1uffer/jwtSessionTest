354a96ae1eee2c7683fce9ae91c9b25da1c8f6bf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-06-14 20:24:48 +0900

    JWT 설정 예제 프로젝트 CustomFilter 추가를 위한 SecurityConfiguration 설정 추가 v2 구현한 JwtAuthenticationFilter 를 등록하는 역할 CustomConfigurer 클래스 생성

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,7 +28,6 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
     }
-
 
     // v1
 //    @Bean
@@ -65,4 +65,11 @@ public class SecurityConfiguration {
 //
 //        return source;
 //    }
+}
+
+class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+    @Override
+    public void configure(HttpSecurity builder) {
+        super.configure(builder);
+    }
 }

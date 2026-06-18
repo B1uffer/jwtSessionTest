f39db75e2340e444c0a919689db510b1d2dfde7c
B1uffer <index.librorum.prohibitorum27@gmail.com>
2026-06-18 13:10:56 +0900

    JWT 설정 예제 프로젝트 예외처리 구현 AuthenticationEntryPoint의 메서드 commence override
    util 메서드 logExceptionMessage() 생성

--- src/main/java/com/example/demo/jwt/handler/MemberAuthenticationEntryPoint.java
+++ src/main/java/com/example/demo/jwt/handler/MemberAuthenticationEntryPoint.java
@@ -1,4 +1,26 @@
 package com.example.demo.jwt.handler;
 
-public class MemberAuthenticationEntryPoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class MemberAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+    }
+
+    private void logExceptionMessage(AuthenticationException authException, Exception exception) {
+
+    }
 }

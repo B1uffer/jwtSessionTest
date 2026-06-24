ac836cc020d809250fbfc71dc6c5d1846ba94d3f
신동진 <index.librorum.prohibitorum27@gmail.com>
2026-06-24 16:30:38 +0900

    JWT 설정 예제 프로젝트 Nimbus를 활용한 토큰기반 인증 JwtTokenizer 교체 코드 generateAccessToken() 메서드 구현

--- src/main/java/com/example/demo/auth/jwt/JwtTokenizer.java
+++ src/main/java/com/example/demo/auth/jwt/JwtTokenizer.java
@@ -1,9 +1,17 @@
 package com.example.demo.auth.jwt;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.Map;
 
 @Component
@@ -17,6 +25,26 @@ public class JwtTokenizer {
     private int accessTokenExpirationMinutes;
 
     public String generateAccessToken(Map<String, Object> claims, String subject) {
-        return null;
+        try {
+            JWSSigner signer = new MACSigner(secretKey.getBytes(StandardCharsets.UTF_8));
+            Date expiration = new Date(System.currentTimeMillis() + accessTokenExpirationMinutes * 60 * 1000);
+
+            JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                    .subject(subject)
+                    .claim("roles", claims.get("roles"))
+                    .expirationTime(expiration)
+                    .issueTime(new Date())
+                    .issuer("example.com")
+                    .build();
+
+            SignedJWT signedJWT = new SignedJWT(
+                    new JWSHeader(JWSAlgorithm.HS256), claimsSet
+            );
+            signedJWT.sign(signer);
+
+            return signedJWT.serialize();
+        } catch(Exception e) {
+            throw new RuntimeException("JWT 발급 실패", e);
+        }
     }
 }
